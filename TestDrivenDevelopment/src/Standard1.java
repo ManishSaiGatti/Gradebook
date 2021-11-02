db9c2d25d0d825dba288df38597223541c9644ce
s-AROTHKOWITZ <s-AROTHKOWITZ@487M089691.lwsd.k12wa.org>
2021-11-02 11:02:34 -0700

    Implemented getScore based off import score set, added many student test

--- TestDrivenDevelopment/src/Standard1.java
+++ TestDrivenDevelopment/src/Standard1.java
@@ -1,12 +1,15 @@
 import java.util.*;
 public class Standard1 {
-	
+	private final int numCorrect85;
+	private final int numCorrect95;
 	private ArrayList<String> questions;
 	private HashMap<String, ArrayList<Boolean>> studentScores;
 	
-	public Standard1() {
+	public Standard1(int numCorrect85, int numCorrect95) {
 		questions = new ArrayList<String>();
 		studentScores = new HashMap<String, ArrayList<Boolean>>();
+		this.numCorrect85 = numCorrect85;
+		this.numCorrect95 = numCorrect95;
 	}
 	public void addQuestions(ArrayList<String> questions) {
 		this.questions = questions;
@@ -25,6 +28,19 @@ public class Standard1 {
 	}
 	
 	public int getScore(String student) {
+		int numCorrect = 0;
+		for(Boolean b: studentScores.get(student)) {
+			if(b) {
+				numCorrect++;
+			}
+		}
+		if(numCorrect < numCorrect85) {
+			return 75;
+		} else if (numCorrect < numCorrect95) {
+			return 85;
+		} else if (numCorrect != questions.size()) {
+			return 95;
+		}
 		return 100;
 	}
 	
--- TestDrivenDevelopment/src/TestStandard1.java
+++ TestDrivenDevelopment/src/TestStandard1.java
@@ -1,14 +1,13 @@
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.*;
 
 import org.junit.Test;
 public class TestStandard1 {
 	
 	@Test
 	public void get1Question() {
-		Standard1 firstStandard = new Standard1();
+		Standard1 firstStandard = new Standard1(0, 0);
 		ArrayList<String> questions = new ArrayList<String>();
 		questions.add("Did the student use conventional indentation and whitespace?");
 		firstStandard.addQuestions(questions);
@@ -18,7 +17,7 @@ public class TestStandard1 {
 	
 	@Test
 	public void get2Question() {
-		Standard1 firstStandard = new Standard1();
+		Standard1 firstStandard = new Standard1(0, 0);
 		ArrayList<String> questions = new ArrayList<String>();
 		questions.add("Did the student use conventional indentation and whitespace?");
 		questions.add("Did the student keep their lines of code under 100 characters in length?");
@@ -29,7 +28,7 @@ public class TestStandard1 {
 	
 	@Test
 	public void get1Score() {
-		Standard1 firstStandard = new Standard1();
+		Standard1 firstStandard = new Standard1(1, 2);
 		ArrayList<String> questions = new ArrayList<String>();
 		questions.add("Did the student use conventional indentation and whitespace?");
 		questions.add("Did the student keep their lines of code under 100 characters in length?");
@@ -44,7 +43,7 @@ public class TestStandard1 {
 	
 	@Test
 	public void testRoster() {
-		Standard1 firstStandard = new Standard1();
+		Standard1 firstStandard = new Standard1(0, 0);
 		ArrayList<String> students = new ArrayList<String>();
 		students.add("John");
 		students.add("Sally");
@@ -63,4 +62,45 @@ public class TestStandard1 {
 		assertTrue("Gave incorrect roster of 3 students", hasSameValues);
 	}
 	
+	@Test
+	public void get2Score() {
+		Standard1 firstStandard = new Standard1(1, 7);
+		ArrayList<String> questions = new ArrayList<String>();
+		questions.add("Did the student use conventional indentation and whitespace?");
+		questions.add("Did the student keep their lines of code under 100 characters in length?");
+		questions.add("Did the student give meaningful names to all their methods and"
+				+ " variables?");
+		questions.add("Did the student follow Java's naming standards?");
+		questions.add("Did the student leave a blank line followed between all methods?");
+		questions.add("Did the student include meaningful comments at the start of each method "
+				+ "other than main?");
+		questions.add("Did the student include short, meaningful comments where appropriate?");
+		questions.add("Did the student complete the header at the top of their submitted .java "
+				+ "files?");
+		questions.add("Did the student use opening and closing curly braces correctly?");
+		
+		firstStandard.addQuestions(questions);
+		ArrayList<Boolean> answers = new ArrayList<Boolean>();
+		for(int i = 0; i < questions.size(); i++) {
+			answers.add(true);
+		}
+		firstStandard.setAnswers("John", answers);
+		assertEquals("Gave incorrect score for 1 student with 100%",
+				firstStandard.getScore("John"), 100);
+		answers.set(0, false);
+		answers.set(1, false);
+		firstStandard.setAnswers("Sally", answers);
+		assertEquals("Gave incorrect score for 1 student with 95%",
+				firstStandard.getScore("Sally"), 95);
+		answers.set(2, false);
+		firstStandard.setAnswers("Tom", answers);
+		assertEquals("Gave incorrect score for 1 student with 85%",
+				firstStandard.getScore("Tom"), 85);
+		for(int i = 3; i < answers.size(); i++) {
+			answers.set(i, false);
+		}
+		firstStandard.setAnswers("Fred", answers);
+		assertEquals("Gave incorrect score for 1 student with 75%",
+				firstStandard.getScore("Fred"), 75);
+	}
 }
