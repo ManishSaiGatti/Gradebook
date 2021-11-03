2ec85bf5b90350704e9122034b3e18cbd2ed3b6a
catherineyu <catherineyu@localhost>
2021-11-03 08:05:46 -0700

    added/edited more methods for standard3

--- TestDrivenDevelopment/src/Standard3.java
+++ TestDrivenDevelopment/src/Standard3.java
@@ -1,36 +1,60 @@
 import java.util.*;
 
 public class Standard3 {
-	// hashmap - key is string, ints for val
-	private HashMap<String, Integer> studentScores;
-	private final int TOTAL_POINTS = 5;
-	private String question;
+	private HashMap<String, Integer> studentRawScores; // key: String studentName, val: int studentRawScore
+	// private final int TOTAL_POINTS = 5;
+	private String question; // eg "how many points out of 5 for ___?"
 	
 	public Standard3(String question) {
 		this.question = question;
-		studentScores = new HashMap<String, Integer>();
+		studentRawScores = new HashMap<String, Integer>();
+	}
+	
+	// adds a new student to hashmap, no score assigned yet (raw score = -1)
+	public void addStudent(String student) {
+		studentRawScores.put(student, -1);
 	}
 	
 	// setAnswers based on student name (void)
-	public void setAsnwers(String student, int rawScore) {
-		studentScores.put(student, rawScore);
+	public void addStudentScore(String student, int rawScore) {
+		studentRawScores.put(student, rawScore);
+	}
+
+	public void editQuestion(String newQuestion) {
+		question = newQuestion;
 	}
 	
 	public void editAnswer(String student, int newRawScore) {
-		studentScores.replace(student, newRawScore);
+		studentRawScores.replace(student, newRawScore);
 	}
 	
-	public int getScore(String student) {
-		// 5-100 4-95 3-85 2-75 1-0
+	public String getQuestion() {
+		return question;
+	}
+	
+	public List<Integer> getStudentScores() {
+		List<Integer> scores = (List<Integer>) studentRawScores.values();
 		
-		if (studentScores.get(student) == null) {
+		for (int i = 0; i < scores.size(); i++) {
+			scores.set(i, convertRawScore((int) scores.get(i)));
+		}		
+		return scores;
+		
+		/*
+		if (studentRawScores.get(student) == null) {
 			throw new IllegalArgumentException(student + " does not exist. "
-						+ "Please add their grade to this standard or try another student.");
+					+ "Please add their grade to this standard or try another student.");
 		}
-		
-		int rawScore = (int) studentScores.get(student);
-		
-		if (rawScore == 5) {
+		 */
+	}
+	
+	// converts raw score to /100 score
+	private int convertRawScore(int rawScore) {
+		// 5-100 4-95 3-85 2-75 1-0
+		if (rawScore == -1) {
+			throw new IllegalArgumentException("You have not given a student "
+					+ "a score yet!"); // IDK IF THIS IS THE RIGHT TYPE OF EXCEPTION TO THROW
+		} else if (rawScore == 5) {
 			return 100;
 		} else if (rawScore == 4) {
 			return 95;
@@ -41,13 +65,13 @@ public class Standard3 {
 		} else if (rawScore == 1) {
 			return 0;
 		} else {
-			throw new IllegalArgumentException("Raw score for " + student +
+			throw new IllegalArgumentException("Raw score for a student" +
 					" was not in the range from 1-5. Please edit their score.");
 		}
 	}
 	
 	// get roster method (List<String>) use keyset,iterator
 	public List<String> getRoster() {
-		return (List<String>) studentScores.keySet().iterator();
+		return (List<String>) studentRawScores.keySet().iterator();
 	}
 }
--- TestDrivenDevelopment/src/Standard3Test.java
+++ TestDrivenDevelopment/src/Standard3Test.java
@@ -3,10 +3,14 @@ import org.junit.Test;
 
 public class Standard3Test {
 	
+	/*
 	@Test
 	public void getTotalPoints1() {
 		int totalPoints = 5;
 		Standard3 s = new Standard3(totalPoints);
 		assertEquals("Total points for standard 3 incorrect", totalPoints, s.getTotalPoints());
 	}
+	*/
+	
+	@Test
 }
