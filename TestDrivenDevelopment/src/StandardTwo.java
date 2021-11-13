58faa05f79ad257f4981a1dbd6899108259a15cd
s-tkamat <s-tkamat@487M089618.lwsd.k12wa.org>
2021-11-13 12:51:49 -0800

    all methods and test cases needed - not all pass yet

--- TestDrivenDevelopment/src/StandardTwo.java
+++ TestDrivenDevelopment/src/StandardTwo.java
@@ -1,62 +1,69 @@
 import java.util.*;
 public class StandardTwo {
 	// eg: "How are you? 75"
-	List<String> questions;
-	ArrayList<Boolean> trueOrFalse;
-	// hashmap for student, arraylist<bboolean> that line up with questions' indices
-	private HashMap<String, ArrayList<Boolean>> studentScore;
-	private List<String> roster;
+	ArrayList<String> questions;
+	// hashmap for student, arraylist<boolean> that line up with questions' indices
+	private HashMap<String, ArrayList<Boolean>> studentAnswers;
+	private double weight;
 	
-	
-	public StandardTwo() {
-		questions = new ArrayList<>();
-		trueOrFalse = new ArrayList<>();
-		studentScore = new HashMap<String, ArrayList<Boolean>>();
-		roster = new ArrayList<>();
+	public static void main(String args[]) {
+		StandardTwo St2 = new StandardTwo(75);
+		/*St2.addIndividualQ("hello?");
+		St2.editQuestions(0, "goodbye?");
+		St2.getQuestions();*/
 	}
 	
+	public StandardTwo(double weight) {
+		questions = new ArrayList<>();
+		studentAnswers = new HashMap<String, ArrayList<Boolean>>();
+		this.weight = weight;
+	}
+	
+	// ADD ANSWERS, STUDENT, ALL QUESTIONS, INDIV QUESTIONS
 	// put question and answers based on student name in hashmap
-	public void setAnswers(String student, String question, boolean correct) {
-		questions.add(question);
-		trueOrFalse.add(correct);
-		studentScore.put(student, trueOrFalse);
+	// only string arraylist
+	public ArrayList<String> addAllQuestions(ArrayList<String> ar) {
+		questions = ar;
+		return questions;
+	}
+	
+	public void addIndividualQ(String q) {
+		questions.add(q);
+	}
+	
+	public void addAllAnswers(String student, ArrayList<Boolean> answers) {
+		studentAnswers.put(student, answers);
+	}
+	
+	public boolean addIndividualA(String student, Boolean a) {
+		studentAnswers.get(student).add(a);
+		System.out.println("answer is " + studentAnswers.get(student).get(-1));
+		return studentAnswers.get(student).get(-1);
 	}
 	
 	public void addStudent(String student) {
 		ArrayList<Boolean> temp = new ArrayList<>();
-		studentScore.put(student, temp);
+		studentAnswers.put(student, temp);
 	}
 	
-	public List<String> getRoster() {
-		// need to return the set itself instead of a list
-		Set<String> rosSet = new HashSet<String>();
-		rosSet = studentScore.keySet();
-		for(String x: rosSet) {
-			roster.add(x);
-		}
-		return roster;
+	// GET ROSTER, 1 STUDENT SCORE, ALL ANSWERS, ALL STUDENT SCORES, ALL QUESTIONS, QUEST WORTH, WEIGHT
+	public Set<String> getRoster() {
+		// make list of roster names
+		return studentAnswers.keySet();
 	}
 	
-	public List<Integer> getAllStudentScores() {
-		ArrayList<Integer> allScores = new ArrayList<>();
-		// for every student, calculate score
-		for(int i = 0; i < roster.size(); i++) {
-			allScores.add(calculateOneStudentTotal(roster.get(i)));
-		}
-		return allScores;
+	public ArrayList<Boolean> getOneStuAnswers(String student) {
+		//System.out.println("ans is " + studentAnswers.get(student).get(0));
+		
+		return studentAnswers.get(student);
 	}
 	
 	public int calculateOneStudentTotal(String student) {
 		int totalScore = 0;
-		for(int i = 0; i < trueOrFalse.size(); i++) {
+		for(int i = 0; i < studentAnswers.get(student).size(); i++) {
 			int worth = getQuestionWorth(i);
-			// if student's answer for the specific question is null
-			if(studentScore.get(student).get(i) == null) {
-				//then break
-				break;
-			}
 			// if student's answer for the specific question is true
-			else if(studentScore.get(student).get(i) == true) {
+			if(studentAnswers.get(student).get(i) == true && worth >= totalScore) {
 				//then the total score is the worth
 				totalScore = worth;
 			}
@@ -64,7 +71,14 @@ public class StandardTwo {
 		return totalScore;
 	}
 	
+	public HashMap<String, ArrayList<Boolean>> getAllStudentAnswers() {
+		return studentAnswers;
+	}
+	
 	public List<String> getQuestions() {
+		for(int i = 0; i < questions.size(); i++) {
+			System.out.println("question is " + questions.get(i));
+		}
 		return questions;
 	}
 	
@@ -73,19 +87,30 @@ public class StandardTwo {
 		int worth = 0;
 		
 		String q = questions.get(index);
-		strScore = q.substring(q.length() - 3); 
-		System.out.println("the score is " + strScore);
+		// get the last 3 characters (the score) & make it a number
+		strScore = q.substring(q.length() - 2);
+		if(strScore.equals("00")) {
+			strScore = "100";
+		}
 		worth = Integer.parseInt(strScore);
-		System.out.println("score is " + worth);
+		System.out.println("the int score is " + worth);
 		return worth;
 	}
 	
+	public double getWeight() {
+		return weight;
+	}
+	
+	
+	// EDIT QUESTIONS, ANSWERS, 
+	// string string, old and new question
 	public void editQuestions(int index, String newQ) {
 		questions.set(index, newQ);
 	}
 	
-	public void editAnswers(int index, boolean newA) {
-		trueOrFalse.set(index, newA);
+	// string string bool
+	public void editAnswers(String student, int index, boolean newA) {
+		studentAnswers.get(student).set(index, newA);
 	}
 	
 }
--- TestDrivenDevelopment/src/TestStandardTwo.java
+++ TestDrivenDevelopment/src/TestStandardTwo.java
@@ -1,12 +1,131 @@
 import static org.junit.Assert.*;
+
+import java.util.*;
 import org.junit.Test;
+
 public class TestStandardTwo {
 	
 	@Test
-	public void testGetScore() {
-		StandardTwo St2 = new StandardTwo();
-		St2.setAnswers("Did you code? @ 75", true);
-		assertEquals("The score is not 0", St2.getScore(), 0);
+	public void testAddAllQuestions() {
+		StandardTwo St2 = new StandardTwo(75);
+		ArrayList<String> ques = new ArrayList<String>();
+		ques.add("Hello? 75");
+		ques.add("who is this? 85");
+		ques.add("where are you? 95");
+		St2.addAllQuestions(ques);
+		assertEquals("The score is not 0", ques, St2.getQuestions());
 	}
+	
+	@Test
+	public void testAddIndividQ() {
+		StandardTwo St2 = new StandardTwo(75);
+		ArrayList<String> qs = new ArrayList<String>();
+		qs.add("How is life? 85");
+		St2.addIndividualQ("How is life? 85");
+		assertEquals("It does not return an array of 1 question", qs, St2.getQuestions());
+	}
+	
+	@Test
+	public void testAddAllAnswers() {
+		StandardTwo St2 = new StandardTwo(75);
+		ArrayList<Boolean> ans = new ArrayList<Boolean>();
+		ans.add(true);
+		ans.add(false);
+		ans.add(true);
+		St2.addAllAnswers("Bob", ans);
+		assertEquals("The score is not 0", ans, St2.getOneStuAnswers("Bob"));
+	}
+	
+	@Test
+	public void testAddIndividA() {
+		StandardTwo St2 = new StandardTwo(75);
+		St2.addIndividualA("Bob", true);
+		assertEquals("The score is not 0", true, St2.getOneStuAnswers("Bob"));
+	}
+	
+	@Test
+	public void testGetQuestionWorth() {
+		StandardTwo St2 = new StandardTwo(75);
+		St2.addIndividualQ("Is it pink? 75");
+		assertEquals("The score is not 75", 75, St2.getQuestionWorth(0));
+	}
+	
+	@Test
+	public void testGetRoster() {
+		StandardTwo St2 = new StandardTwo(75);
+		ArrayList<Boolean> ans = new ArrayList<Boolean>();
+		ans.add(true);
+		ans.add(false);
+		ans.add(true);
+		St2.addAllAnswers("Bob", ans);
+		ArrayList<Boolean> ans2 = new ArrayList<Boolean>();
+		ans.add(false);
+		ans.add(true);
+		ans.add(false);
+		St2.addAllAnswers("Eric", ans2);
+		List<String> ros = new ArrayList<String>();
+		ros.add("Bob");
+		ros.add("Eric");
+		//assertEquals("The score is not 75", St2.getRoster(), ros);
+		assertTrue(St2.getRoster().contains("Eric"));
+	}
+	
+	@Test
+	public void testEditQ() {
+		StandardTwo St2 = new StandardTwo(75);
+		St2.addIndividualQ("hello?");
+		St2.editQuestions(0, "goodbye?");
+		assertEquals("The question didn't edit correctly", "goodbye?", St2.getQuestions());
+	}
+	
+	@Test
+	public void testEditA() {
+		StandardTwo St2 = new StandardTwo(75);
+		HashMap<String, ArrayList<Boolean>> studentAnswer = new HashMap<String, ArrayList<Boolean>>();
+		ArrayList<Boolean> ans = new ArrayList<Boolean>();
+		ans.add(true);
+		studentAnswer.put("Bob", ans);
+		ArrayList<Boolean> ans2 = new ArrayList<Boolean>();
+		ans2.add(false);
+		studentAnswer.put("Mary", ans2);
+		St2.editAnswers("Mary", 0, true);
+		assertEquals("hashmap of all student answers is wrong", studentAnswer, St2.getAllStudentAnswers());
+	}
+	
+	@Test
+	public void testGetQs() {
+		StandardTwo St2 = new StandardTwo(75);
+		ArrayList<String> ques = new ArrayList<String>();
+		ques.add("Hello? 75");
+		ques.add("who is this? 85");
+		ques.add("where are you? 95");
+		St2.addAllQuestions(ques);
+		assertEquals("the questions came out wrong", ques, St2.getQuestions());
+	}
+	
+	@Test
+	public void testGetAllA() {
+		StandardTwo St2 = new StandardTwo(75);
+		HashMap<String, ArrayList<Boolean>> studentAnswer = new HashMap<String, ArrayList<Boolean>>();
+		ArrayList<Boolean> ans = new ArrayList<Boolean>();
+		ans.add(true);
+		studentAnswer.put("Bob", ans);
+		ArrayList<Boolean> ans2 = new ArrayList<Boolean>();
+		ans2.add(false);
+		studentAnswer.put("Mary", ans2);
+		assertEquals("hashmap of all student answers is wrong", studentAnswer, St2.getAllStudentAnswers());
+	}
+	
+	@Test
+	public void testGetOneA() {
+		StandardTwo St2 = new StandardTwo(75);
+		HashMap<String, ArrayList<Boolean>> studentAnswer = new HashMap<String, ArrayList<Boolean>>();
+		ArrayList<Boolean> ans = new ArrayList<Boolean>();
+		ans.add(true);
+		studentAnswer.put("Bob", ans);
+		System.out.println("ans is " + St2.getOneStuAnswers("Bob").get(0));
+		assertEquals("hashmap with only one student answers is wrong", ans, St2.getOneStuAnswers("Bob"));
+	}
+	
 	
 }
